d07910bf04ef4a2543cbbb7ea26e93bdb7377bee
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-15 13:46:01 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,43 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand=brand
+    end
+
+    def brand(brand)
+        @brand = brand
+    end
+    
+    def color(color)
+        @color = color
+    end
+
+    def size(num)
+        @size = num
+    end
+
+    def material(type) 
+        @material = type
+    end
+    
+    def brand
+        @brand
+    end
+
+    def color
+        @color
+    end
+    
+    def size
+        @size
+    end
+
+    def material
+        @material
+    end
+
+
+
+
+
+end
