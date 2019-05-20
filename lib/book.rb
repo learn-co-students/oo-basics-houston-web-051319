c3bb0b905dad53f8b377b555f9d12665b9bd3b1f
aruande <kalunga212@gmail.com>
2019-05-20 01:15:14 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,55 @@
+class Book
 
+    def initialize(title)
+        @title = title
+        
+    end
+
+    def title
+        @title
+
+    end
+
+    def author=(name)
+        @name = name
+    end
+
+    def author
+        @name
+    end
+
+    def page_count=(num)
+        @num = num
+    end
+
+    def page_count
+        @num
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+end
 
